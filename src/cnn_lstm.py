3c6de3a0fe3682d0e9c73ce4d8764986706749df
kesl.radomir <kesl.radomir@tuta.io>
2024-04-30 18:10:38 +0200

    Batch norm, grouping, fixed acc

--- src/cnn_lstm.py
+++ src/cnn_lstm.py
@@ -3,82 +3,76 @@ import torch
 import torch.nn.functional as F
 from torch import nn
 from torch.optim import Adam
+from torchmetrics import Accuracy
 
 
 class CNN_LSTM_1D(L.LightningModule):
+
     def __init__(
         self,
         data_shape,
         in_channels,
+        conv1_kernel = 5,
+        conv2_kernel = 7,
+        conv1_filters = 64,
+        conv2_filters = 128,
+        pool_kernel = 4,
         hidden_size=64,
         feature_count=4,
     ):
         super().__init__()
-        self.conv1 = nn.Conv1d(
-            in_channels=in_channels,
-            out_channels=64,
-            kernel_size=5,
+        self.conv1 = nn.Sequential(
+            nn.Conv1d(
+                in_channels=in_channels,
+                kernel_size=conv1_kernel,
+                out_channels=conv1_filters,
+                padding='same',
+            ),
+            nn.BatchNorm1d(conv1_filters),
+            nn.ReLU(),
+            nn.MaxPool1d(kernel_size=pool_kernel, stride=1),
         )
-        self.conv2 = nn.Conv1d(
-            in_channels=64,
-            out_channels=128,
-            kernel_size=7,
+        self.conv2 = nn.Sequential(
+            nn.Conv1d(
+                in_channels=conv1_filters,
+                kernel_size=conv2_kernel,
+                out_channels=conv2_filters,
+                padding='same'
+            ),
+            nn.BatchNorm1d(conv2_filters),
+            nn.ReLU(),
+            nn.MaxPool1d(kernel_size=pool_kernel, stride=1),
         )
-        self.pool = nn.MaxPool1d(kernel_size=4, stride=1)
-        # self.input_to_hidden = nn.Linear(in_features=self.feature_count_after_pools(data_shape), out_features=hidden_size)
-        self.hidden_to_output = nn.Linear(
+        self.lstm = nn.LSTM(
+                input_size=self.feature_count_after_convs(data_shape),
+                hidden_size=hidden_size,
+        )
+        self.full = nn.Linear(
             in_features=hidden_size, out_features=feature_count
         )
         self.loss = nn.CrossEntropyLoss()
-        self.lstm = nn.LSTM(
-            input_size=self.feature_count_after_pools(data_shape),
-            hidden_size=hidden_size,
-        )
+        self.accuracy = Accuracy(task='multiclass', num_classes=4)
 
     def forward(self, x):
-        # print('Forwarding...')
-        # print(f'Input shape: {x.shape}')
-        # Convolve once
         x = self.conv1(x)
-        # print(f'After first conv: {x.shape}')
-        x = F.relu(x)
-        x = self.pool(x)
-        # print(f'After first pooling: {x.shape}')
-
-        # Convolve again
         x = self.conv2(x)
-        # print(f'After second conv: {x.shape}')
-        x = F.relu(x)
-        x = self.pool(x)
-        # print(f'After second pooling: {x.shape}')
 
         x = torch.flatten(x, 1)
-        # print(f'After flattening: {x.shape}')
-
-        # x = self.input_to_hidden(x)
-        # print(f'After input to hidden: {x.shape}')
-        # x = F.relu(x)
         x, _ = self.lstm(x)
-        x = self.hidden_to_output(x)
-        # print(f'After hidden to output: {x.shape}')
+
+        x = self.full(x)
 
         return x
 
-    def feature_count_after_pools(self, data_shape):
+    def feature_count_after_convs(self, data_shape):
         x = torch.zeros(data_shape)
         x = self.conv1(x)
-        x = F.relu(x)
-        x = self.pool(x)
-
         x = self.conv2(x)
-        x = F.relu(x)
-        x = self.pool(x)
 
-        count = x.numel()
-        return count
+        return x.numel()
 
     def configure_optimizers(self):
-        return Adam(self.parameters(), lr=0.01)
+        return Adam(self.parameters(), lr=0.001)
 
     def training_step(self, batch, batch_idx):
         inputs, labels = batch
@@ -87,6 +81,20 @@ class CNN_LSTM_1D(L.LightningModule):
         # print(f'Outputs: {outputs}\nLabels: {labels}')
         loss = self.loss(outputs, labels)
         return loss
+
+    def validation_step(self, batch, batch_idx):
+        inputs, labels = batch
+        outputs = self.forward(inputs)
+        val_loss = self.loss(outputs, labels)
+        self.log('val_loss', val_loss)
+
+    def test_step(self, batch, batch_idx):
+        inputs, labels = batch
+        outputs = self.forward(inputs)
+        test_loss = self.loss(outputs, labels)
+        self.accuracy(outputs, torch.argmax(labels, dim=1))
+        self.log('test_acc', self.accuracy, on_step=False, on_epoch=True)
+        self.log('test_loss', test_loss)
 
 
 class CNN_LSTM_2D(L.LightningModule):
@@ -174,10 +182,12 @@ class CNN_LSTM_2D(L.LightningModule):
         return loss
 
 
+
 if __name__ == "__main__":
-    from loader import *
+    from pytorch_lightning.callbacks.early_stopping import EarlyStopping
     from torch.utils.data import random_split
 
+    from loader import *
 
     L.seed_everything(seed=42, workers=True)
 
@@ -191,22 +201,16 @@ if __name__ == "__main__":
     # )
     # tds = make_dataset(ds)
     tds: TensorDataset = torch.load('sub01_ses05-11_chansC.ds')
-    sets = random_split(tds, [0.8, 0.2])
-    train, test = tuple(DataLoader(s, num_workers=3, batch_size=16) for s in sets)
+    sets = random_split(tds, [0.64, 0.16, 0.2])
+    train, val, test = tuple(DataLoader(s, num_workers=3, batch_size=16) for s in sets)
 
     model = CNN_LSTM_1D(
         tds[0][0].shape,
         in_channels=len(used_channels)
     )
 
-    trainer = L.Trainer(max_epochs=50)
-    trainer.fit(model, train_dataloaders=train)
-    correct = 0
-    for batch_num, (trial, label) in enumerate(test):
-        pred = model(trial)
-        pred_label = torch.round(torch.softmax(pred, dim=1), decimals=2)
-        print(f"Predicted label:\t{pred_label}\tTrue label:\t{label}")
-        if torch.argmax(pred_label) == torch.argmax(label):
-            correct += 1
-    print(f"Accuracy: {correct / len(test)}")
+    trainer = L.Trainer(max_epochs=10, callbacks=[EarlyStopping(monitor='val_loss', mode='min', patience=3)])
+    trainer.fit(model, train_dataloaders=train, val_dataloaders=val)
+    trainer.test(model, test)
     trainer.save_checkpoint("cnn_lstm.ckpt")
+
