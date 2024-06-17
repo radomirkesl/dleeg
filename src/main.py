21d2c003e5eae246cb8f8d2aadc479637aa576d8
kesl.radomir <kesl.radomir@tuta.io>
2024-06-17 16:07:59 +0200

    Restored cnn

--- src/main.py
+++ src/main.py
@@ -2,6 +2,7 @@ from sys import argv
 
 import torch
 
+from cnn import CNN
 from cnn_lstm import CNN_LSTM
 from loader import *
 from run import Runner
@@ -9,7 +10,7 @@ from run import Runner
 if __name__ == "__main__":
 
     ds: TensorDataset = torch.load(argv[1])
-    model = CNN_LSTM(data_shape=ds.tensors[0][0].shape)
+    model = CNN(data_shape=ds.tensors[0][0].shape, in_channels=ds.tensors[0][0].shape[0])
     if len(argv) > 2:
         model_save = argv[2]
     else:
